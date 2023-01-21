200a9ade15036d8390dcb8c444f9e06ea585016f
ahyaghifari <ahyaghifari288@gmail.com>
2023-01-21 10:16:55 +0800

    update settings

--- deluxe/settings.py
+++ deluxe/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://deluxe.up.railway.app/', '127.0.0.1']
+ALLOWED_HOSTS = ['deluxe.up.railway.app', '127.0.0.1']
 MY_HOST = "https://deluxe.up.railway.app"
 
 # Application definition
