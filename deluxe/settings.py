baa622b36cf87f8489eb225a6b8341358f96c1ee
ahyaghifari <ahyaghifari288@gmail.com>
2023-01-21 12:19:57 +0800

    third commit

--- deluxe/settings.py
+++ deluxe/settings.py
@@ -10,7 +10,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
