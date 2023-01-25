85639ad20bb1a7e9567940f80c2af789b91efc35
ahyaghifari <ahyaghifari288@gmail.com>
2023-01-26 00:26:48 +0800

    nav toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,15 @@ $(document).ready(function () {
         $(this).toggleClass('active')
         $('#nav-list').toggleClass('active')
     })
+    var nav = document.getElementsByTagName('nav');
+    var navtoggle = document.getElementById('nav-toggle');
+    var navlist = document.getElementById('nav-list');
+
+    $(document).on('click', function (e) {
+        if (!$(e.target).closest('nav').length) {
+            $('#nav-toggle').removeClass('active')
+            $('#nav-list').removeClass('active')
+        }
+    })
+
 });
